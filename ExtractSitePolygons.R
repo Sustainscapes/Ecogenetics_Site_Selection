bc7512f632d88faf47726830b04bea5146b4e06e
derek-corcoran-barrios <derek-corcoran-barrios@gmail.com>
2023-04-28 13:59:42 +0200

    Created the categories for grassland

--- ExtractSitePolygons.R
+++ ExtractSitePolygons.R
@@ -1,3 +1,7 @@
 library(terra)
 library(tidyverse)
 library(foreign)
+
+Grasslands <- c("Frit areal (overdrev)", "Græs-indlandsklit", "Kalkoverdrev", "Overdrev", "Slette, Overdrev (græsset)","Slette, Overdrev (overdrev)", "Slette, Overdrev (Slette)", "Surt overdrev", "tør overdrev på kalkholdigt sand", "skrænt")
+
+PlotsToExtract <- read_csv("GrasslandsALL_INFO.csv") |> dplyr::select(akt_id, lon, lat)
